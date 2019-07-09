96085f5da4fec7161d75d6f19da0dc0dfae9a9b2
baguramu <mototoshi.t0422@gmail.com>
2019-07-09 14:41:11 +0900

    試しのコミット。orgeの中に。を付けました

--- pokopen/Assets/MainScript/GM_PlayerController.cs
+++ pokopen/Assets/MainScript/GM_PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GM_PlayerController : CharactorBaseController
 {
-    //Rayを使うときに当たったオブジェクトを取っておくためのもの
+    //Rayを使うときに当たったオブジェクトを取っておくためのもの。
     RaycastHit Hit;
     public override void Start()
     {
